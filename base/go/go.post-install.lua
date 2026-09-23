d7e92ae1f954d10a0eaf13aee7f95cc82009a957
tnut <tnut@nutyx.org>
2026-09-23 10:19:32 +0200

    go 1.27.1-2

---
+++ base/go/go.post-install.lua
@@ -0,0 +1,5 @@
+-- https://github.com/kikito/ansicolors.lua/
+INFO="\27[1;36m"
+NORMAL="\27[0;39m"
+
+print(INFO .. "go application is in /usr/lib/go/bin. Make sure to include it in PATH" .. NORMAL)
